bcf67aaa6df1c34d6a170f53055242de14113727
Espen A. Kristiansen <post@espenak.net>
2015-11-19 08:31:32 +0100

    ievvtasks_common.cli: Handle shell escaping in unknown_args.

--- ievv_opensource/ievvtasks_common/cli.py
+++ ievv_opensource/ievvtasks_common/cli.py
@@ -1,10 +1,17 @@
 import argparse
 import logging
 import os
+import re
 import sys
 import textwrap
 
 import sh
+
+try:
+    from shlex import quote as cmd_quote
+except ImportError:
+    from pipes import quote as cmd_quote
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,
@@ -35,10 +42,49 @@ def _make_cli_epilog(commands):
     return cli_help
 
 
+class UnknownArgsParser(object):
+    short_option_pattern = re.compile(
+        '^-(?P<option>[a-zA-Z0-9][a-zA-Z0-9_-]*)=(?P<value>.+)$')
+    long_option_pattern = re.compile(
+        '^--(?P<option>[a-zA-Z0-9][a-zA-Z0-9_-]*)=(?P<value>.+)$')
+
+    def __init__(self, unknown_args):
+        self.unknown_args = unknown_args
+        self.fixed_unknown_args = self.__parse()
+
+    def __parse_argument(self, argument):
+        long_option_match = self.long_option_pattern.match(argument)
+        short_option_match = self.short_option_pattern.match(argument)
+        if long_option_match:
+            groupdict = long_option_match.groupdict()
+            return '--{option}={value}'.format(
+                option=groupdict['option'],
+                value=cmd_quote(groupdict['value']),
+            )
+        elif short_option_match:
+            groupdict = short_option_match.groupdict()
+            return '-{option}={value}'.format(
+                option=groupdict['option'],
+                value=cmd_quote(groupdict['value']),
+            )
+        else:
+            return cmd_quote(argument)
+
+    def __parse(self):
+        fixed_unknown_args = []
+        for argument in self.unknown_args:
+            fixed_unknown_args.append(self.__parse_argument(argument))
+        return fixed_unknown_args
+
+    def resultlist(self):
+        return self.fixed_unknown_args
+
+    def resultstring(self):
+        return ' '.join(self.fixed_unknown_args)
+
+
 def cli():
     commands = _find_management_commands()
-    # commands.extend([
-    # ])
     commands = list(set(commands))
     commands.sort()
 
@@ -67,5 +113,6 @@ def cli():
         #     # parser.add_argument('command', type=str)
         #     # args = parser.parse_args(unknown_args)
         # else:
+        parsed_unknown_args = UnknownArgsParser(unknown_args)
         os.system('python manage.py ievvtasks_{} {}'.format(
-            args.command, ' '.join(unknown_args)))
+            args.command, parsed_unknown_args.resultstring()))
