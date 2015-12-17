1b000dc882b47debaeaa9e25190943f1b4aa93ae
Espen A. Kristiansen <post@espenak.net>
2015-12-17 10:55:38 +0100

    devrun: Handle sigterm.

--- ievv_opensource/utils/ievvdevrun/config.py
+++ ievv_opensource/utils/ievvdevrun/config.py
@@ -1,5 +1,8 @@
 import time
 
+import sys
+import signal
+
 
 class RunnableThreadList(object):
     def __init__(self, *runnablethreads):
@@ -11,15 +14,36 @@ class RunnableThreadList(object):
         self._runnablethreads.append(runnablethread)
 
     def start(self):
-        for runnablethread in self._runnablethreads:
-            runnablethread.start()
+        def start_all():
+            for runnablethread in self._runnablethreads:
+                runnablethread.start()
 
+        def stop_all():
+            for runnablethread in self._runnablethreads:
+                runnablethread.stop()
+
+        def join_all():
+            for runnablethread in self._runnablethreads:
+                runnablethread.join()
+
+        def on_terminate(*args):
+            print('', file=sys.stderr)
+            print('*' * 70, file=sys.stderr)
+            print('ievvdevrun terminated', file=sys.stderr)
+            print('*' * 70, file=sys.stderr)
+            stop_all()
+            join_all()
+            sys.exit(0)
+
+        start_all()
+        signal.signal(signal.SIGTERM, on_terminate)
         try:
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
-            for runnablethread in self._runnablethreads:
-                runnablethread.stop()
-
-        for runnablethread in self._runnablethreads:
-            runnablethread.join()
+            print('', file=sys.stderr)
+            print('*' * 70, file=sys.stderr)
+            print('ievvdevrun KeyboardInterrupt', file=sys.stderr)
+            print('*' * 70, file=sys.stderr)
+            stop_all()
+        join_all()
