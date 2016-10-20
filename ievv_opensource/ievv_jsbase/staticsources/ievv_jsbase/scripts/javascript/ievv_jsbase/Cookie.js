8f78fc832626799f0222894da93dae8a5f1f6514
Espen A. Kristiansen <post@espenak.net>
2016-10-20 18:47:32 +0200

    ievv_jsbase: Tests for Cookie.

--- ievv_opensource/ievv_jsbase/staticsources/ievv_jsbase/scripts/javascript/ievv_jsbase/Cookie.js
+++ ievv_opensource/ievv_jsbase/staticsources/ievv_jsbase/scripts/javascript/ievv_jsbase/Cookie.js
@@ -4,11 +4,11 @@ class Cookie {
     }
 
     getValue() {
-        var cookieValue = null;
+        let cookieValue = null;
         if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = cookies[i].trim();
+            let cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                let cookie = cookies[i].trim();
                 // Does this cookie string begin with the name we want?
                 if (cookie.substring(0, this.cookieName.length + 1) === (this.cookieName + '=')) {
                     cookieValue = decodeURIComponent(cookie.substring(this.cookieName.length + 1));
--- ievv_opensource/ievv_jsbase/staticsources/ievv_jsbase/scripts/javascript/ievv_jsbase/__tests__/Cookie-tests.js
+++ ievv_opensource/ievv_jsbase/staticsources/ievv_jsbase/scripts/javascript/ievv_jsbase/__tests__/Cookie-tests.js
@@ -1,7 +1,21 @@
 import Cookie from '../Cookie';
 
 
-test('Cookie("test").cookieName to be "test"', () => {
-    const cookie = new Cookie('test');
-    expect(cookie.cookieName).toBe('test');
+describe('Cookie', () => {
+    it('Cookie("test").cookieName to be "test"', () => {
+        const cookie = new Cookie('test');
+        expect(cookie.cookieName).toBe('test');
+    });
+
+    it('Cookie.getValue() simple', () => {
+        document.cookie = 'test=testvalue';
+        const cookie = new Cookie('test');
+        expect(cookie.getValue()).toBe('testvalue');
+    });
+
+    it('Cookie.getValue() multiple cookies', () => {
+        document.cookie = 'other=othervalue;test=testvalue;evenanother=evenanothervalue';
+        const cookie = new Cookie('test');
+        expect(cookie.getValue()).toBe('testvalue');
+    });
 });
