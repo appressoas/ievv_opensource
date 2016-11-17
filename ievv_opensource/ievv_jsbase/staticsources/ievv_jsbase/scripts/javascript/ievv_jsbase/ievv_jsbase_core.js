9fa27b8f0f3616c5b1304c79fbace90558b478ae
Espen A. Kristiansen <post@espenak.net>
2016-11-17 02:39:58 +0100

    jsbase: Add Array.from polyfill for IE11 compatibility.

--- ievv_opensource/ievv_jsbase/staticsources/ievv_jsbase/scripts/javascript/ievv_jsbase/ievv_jsbase_core.js
+++ ievv_opensource/ievv_jsbase/staticsources/ievv_jsbase/scripts/javascript/ievv_jsbase/ievv_jsbase_core.js
@@ -1,6 +1,11 @@
 import SignalHandlerSingleton from "./SignalHandlerSingleton";
 import WidgetRegistrySingleton from "./widget/WidgetRegistrySingleton";
 import LoggerSingleton from "./log/LoggerSingleton";
+import arrayFromPolyfill from "./polyfill/arrayFromPolyfill";
+
+
+// Array.from polyfill for IE11 compatibility
+arrayFromPolyfill();
 
 window.ievv_jsbase_core = {
     SignalHandlerSingleton: SignalHandlerSingleton,
