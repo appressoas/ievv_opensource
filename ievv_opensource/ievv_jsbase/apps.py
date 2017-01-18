ebd78f0b58246dc66f46e204131157b9610a3bde
Espen A. Kristiansen <post@espenak.net>
2017-01-18 18:11:10 +0100

    jsbase: Add a basic AppConfig.

--- ievv_opensource/ievv_jsbase/apps.py
+++ ievv_opensource/ievv_jsbase/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-from django_cradmin import javascriptregistry
-from ievv_opensource.ievv_jsbase import static_components
 
 
 class IevvJsBaseAppConfig(AppConfig):
@@ -8,5 +6,15 @@ class IevvJsBaseAppConfig(AppConfig):
     verbose_name = "IEVV jsbase"
 
     def ready(self):
+        from django_cradmin import javascriptregistry
+        from ievv_opensource.ievv_jsbase import static_components
         javascriptregistry.Registry.get_instance().add(
             static_components.IevvJsBaseCoreComponent)
+
+
+class IevvJsBaseBasicAppConfig(AppConfig):
+    """
+    Basic appconfig without any automatic registry updates.
+    """
+    name = 'ievv_opensource.ievv_jsbase'
+    verbose_name = "IEVV jsbase"
