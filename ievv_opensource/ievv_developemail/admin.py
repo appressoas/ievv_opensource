2755a0072f7732eb9ed1123a5b75283f6cb2ea88
Espen A. Kristiansen <t@espenak.net>
2018-04-30 14:18:27 +0200

    developemail: All the low level stuff in a usable state.

--- ievv_opensource/ievv_developemail/admin.py
+++ ievv_opensource/ievv_developemail/admin.py
@@ -4,17 +4,23 @@ from ievv_opensource.ievv_developemail.models import DevelopEmail
 
 
 @admin.register(DevelopEmail)
-class DevelopEmail(admin.ModelAdmin):
+class DevelopEmailAdmin(admin.ModelAdmin):
     list_display = [
         'id',
+        'subject',
+        'from_email',
+        'recipients',
         'created_datetime',
     ]
 
     search_fields = [
         'id',
+        'from_email',
+        'recipients',
         'raw_message',
     ]
 
     list_filter = [
         'created_datetime',
+        'from_email',
     ]
--- ievv_opensource/ievv_developemail/email_backend.py
+++ ievv_opensource/ievv_developemail/email_backend.py
@@ -1,20 +1,31 @@
+import logging
+
 from django.core.mail.backends.base import BaseEmailBackend
+from django.utils.encoding import force_text
 
 from ievv_opensource.ievv_developemail.models import DevelopEmail
+
+
+logger = logging.getLogger(__name__)
 
 
 class DevelopEmailBackend(BaseEmailBackend):
     def send_messages(self, email_messages):
         developemails = []
-        for message in email_messages:
-            developemail = DevelopEmail(raw_content=str(message))
-            print()
-            print("*" * 70)
-            print()
-            print(dir(message))
-            print()
+        for email_message in email_messages:
+            # message = email_message.message()
+            raw_message = email_message.message().as_string()
+            developemail = DevelopEmail(
+                subject=email_message.subject,
+                from_email=email_message.from_email,
+                recipients=', '.join(map(force_text, email_message.to)),
+                raw_message=raw_message
+            )
 
-
+            # emailmessage = email.message_from_string(message.message().as_string())
             developemails.append(developemail)
-        # DevelopEmail.objects.bulk_create(developemails)
+
+            logger.debug(raw_message)
+
+        DevelopEmail.objects.bulk_create(developemails)
         return len(list(email_messages))
