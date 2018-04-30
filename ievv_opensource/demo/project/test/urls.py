d9f61e6b0b7489e7856c8c55c61d0f73d8c74c33
Espen A. Kristiansen <t@espenak.net>
2018-04-30 12:46:18 +0200

    demo.projecet.test.urls: Update for newer django url style.

--- ievv_opensource/demo/project/test/urls.py
+++ ievv_opensource/demo/project/test/urls.py
@@ -1,8 +1,4 @@
-from django.conf.urls import patterns
 
 from ievv_opensource.demo.project.default.default_urls import default_urls
 
-urlpatterns = patterns(
-    '',
-    *default_urls
-)
+urlpatterns = [] + default_urls
