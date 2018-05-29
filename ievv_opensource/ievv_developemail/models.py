ed25f49603d5a16913a3083db60824dcedb7fc73
Stian Julseth <stianjul@gmail.com>
2018-05-29 17:25:51 +0200

    ievv_developemail: Fix minor python 2.x compatibility issues.

--- ievv_opensource/ievv_developemail/models.py
+++ ievv_opensource/ievv_developemail/models.py
@@ -2,9 +2,7 @@ try:
     from email import message_from_bytes
 except ImportError:
     # For python 2.x compatibility
-    import email
-    def message_from_bytes(bytestring):
-        return email.message_from_string(bytestring.decode('utf-8'))
+    from future.backports.email import message_from_bytes
 
 from collections import OrderedDict
 
