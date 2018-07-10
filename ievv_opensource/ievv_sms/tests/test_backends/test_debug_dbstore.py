f1566e78f0e151c92bfe406495236b90bc5a414f
Espen A. Kristiansen <t@espenak.net>
2018-07-10 16:46:06 +0200

    ievv_sms: Fix PEP8 error.

--- ievv_opensource/ievv_sms/tests/test_backends/test_debug_dbstore.py
+++ ievv_opensource/ievv_sms/tests/test_backends/test_debug_dbstore.py
@@ -6,7 +6,7 @@ from ievv_opensource.ievv_sms.models import DebugSmsMessage
 
 class TestBackend(test.TestCase):
     def test_sms_record_is_created_in_db(self):
-        debug = debug_dbstore.Backend(message="Hi there!",phone_number="1234567")
+        debug = debug_dbstore.Backend(message="Hi there!", phone_number="1234567")
         debug.send()
         self.assertEqual(
             DebugSmsMessage.objects.first().message,
