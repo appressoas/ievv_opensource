86f6eaab06d92c58a8eeaf44f50e92eda5b79858
Espen A. Kristiansen <t@espenak.net>
2020-04-22 12:25:52 +0200

    docs.conf: Add root directory to sys.path.

--- not_for_deploy/docs/conf.py
+++ not_for_deploy/docs/conf.py
@@ -10,14 +10,15 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
-import sys, os
+import sys
+import os
 import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ievv_opensource.demo.project.settingsproxy")
 import django
