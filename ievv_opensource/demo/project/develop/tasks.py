725ef477717a940e338baeefd4ba2f3093079dce
Espen Kristiansen <espen@appresso.no>
2023-03-31 22:27:06 +0200

    fix: fix rq task that use python2 style print statement

--- ievv_opensource/demo/project/develop/tasks.py
+++ ievv_opensource/demo/project/develop/tasks.py
@@ -3,5 +3,5 @@ from django_rq import job
 
 @job
 def add(x, y):
-    print 'Add was called! with x:{}, y:{}'.format(x, y)
+    print('Add was called! with x:{}, y:{}'.format(x, y))
     return x + y
